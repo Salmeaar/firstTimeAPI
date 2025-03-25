06cf5ca5216928fac4db91d57d8666b01ccf5389
Aaro Salmela <salmeaar@arcada.fi>
2025-03-25 10:04:11 +0000

    ip keikor

--- publicIP/main.py
+++ publicIP/main.py
@@ -1,15 +1,18 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 
-PORT=8390
+PORT=8391
 
 app = FastAPI()
 app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
 
 @app.get("/")
-def hello():
-    return { "message": "Hello FastAPI" }
+def read_root(request: Request):
+    
+    client_host = request.client.host
+    return {"client_host": client_host}
+    
 
 if __name__ == "__main__":
     uvicorn.run(
@@ -19,3 +22,5 @@ if __name__ == "__main__":
         ssl_keyfile="/etc/letsencrypt/privkey.pem",
         ssl_certfile="/etc/letsencrypt/fullchain.pem",
     )
+
+
