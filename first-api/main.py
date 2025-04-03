e0e81424ae5dc18280cc5c2c421a85271a2b1c61
Aaro Salmela <salmeaar@arcada.fi>
2025-04-03 08:00:51 +0000

    mellan commit fö hotels

--- first-api/main.py
+++ first-api/main.py
@@ -7,6 +7,33 @@ PORT=8390
 app = FastAPI()
 app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
 
+@app.get("/if/{user_input}")
+def if_test(user_input: str):
+    message = None #None = Null 
+    if user_input == "hello" or user_input == "hi":
+        
+        message = user_input + " yourself!"
+    elif user_input == "goodbye":
+        message = "bye bye"
+    else:
+        message = f"i didn't understand! {user_input}"
+    return {"msg": message}
+
+@app.get("/temp")
+def temp():
+    with conn.cursor() as cur:
+        cur.execute("SELECT * FROM messages")
+        messages = cur.fetchall()
+        return messages
+    
+@app.get("/tempp{}")
+def temp():
+    with conn.cursor() as cur:
+        cur.execute("SELECT * FROM messages")
+        messages = cur.fetchall()
+        return messages
+
+
 @app.get("/")
 def hello():
     msg ="Några populära SOA-implementationer" #string variabel
--- hotel/main.py
+++ hotel/main.py
@@ -3,6 +3,7 @@ from psycopg.rows import dict_row
 from dotenv import load_dotenv
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
 
 PORT=8390
 
@@ -10,7 +11,7 @@ PORT=8390
 load_dotenv()
 DB_URL = os.getenv("DB_URL")
 
-print(DB_URL)
+#print(DB_URL)
 # Create DB connection
 conn = psycopg.connect(DB_URL, autocommit=True, row_factory=dict_row)
 
@@ -18,47 +19,53 @@ conn = psycopg.connect(DB_URL, autocommit=True, row_factory=dict_row)
 app = FastAPI()
 app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
 
+class Booking(BaseModel):
+    guest_id: int
+    room_id: int
+    
 
 
-
-@app.get("/temp")
-def temp():
-    with conn.cursor() as cur:
-        cur.execute("SELECT * FROM messages")
-        messages = cur.fetchall()
-        return messages
-
-
-rooms = [
-    {"nummer":"101","type":"single","status":"available"},
-    {"nummer":"102","type":"single","status":"occupied"},
-    {"nummer":"103","type":"double","status":"available"},
-    {"nummer":"104","type":"double","status":"occupied"},
-    {"nummer":"105","type":"triple","status":"available"},
-    {"nummer":"106","type":"triple","status":"occupied"},
-    {"nummer":"201","type":"suite","status":"available"},
-    {"nummer":"202","type":"suite","status":"occupied"},
-]
+# rooms = [
+#     {"nummer":"101","type":"single","status":"available"},
+#     {"nummer":"102","type":"single","status":"occupied"},
+#     {"nummer":"103","type":"double","status":"available"},
+#     {"nummer":"104","type":"double","status":"occupied"},
+#     {"nummer":"105","type":"triple","status":"available"},
+#     {"nummer":"106","type":"triple","status":"occupied"},
+#     {"nummer":"201","type":"suite","status":"available"},
+#     {"nummer":"202","type":"suite","status":"occupied"},
+# ]
 
 #all rooms
 @app.get("/rooms/")
 def vacants():
-    with conn.cursor() as cur:
-        cur.execute("SELECT * FROM hotel_rooms")
-        messages = cur.fetchall()
+        with conn.cursor() as cur:
+            cur.execute("SELECT * FROM hotel_rooms")
+            messages = cur.fetchall()
         return messages
     
 #one room
 @app.get("/rooms/{id}")
 def vacant_room(id: int):
-    try:
-        return rooms[id]
-    except:
-        return{"error": "Room not found"}
+    with conn.cursor() as cur:
+        cur.execute("SELECT * FROM hotel_rooms WHERE id = %s", [id])
+        #cur.execute("SELECT * FROM hotel_rooms WHERE id = %s", (id,)
+        #cur.execute("SELECT * FROM hotel_rooms WHERE id = %(id)s", {"id":id})
+        room = cur.fetchone()
+        if not room:
+            return{"msg":"Room not found"}
+        return room
+    
 
 @app.post("/bookings")
-def create_booking(request: Request):
-    return {"msg": "booking created!"}
+def create_booking(booking: Booking):
+    with conn.cursor() as cur:
+        cur.execute("""INSERT INTO hotel_booking
+                    (guest_id,room_id)
+                    VALUES(%s,%s) RETURNING id
+                    """,[booking.guest_id, booking.room_id])
+        new_id = cur.fetchone()['id']
+    return {"msg": "booking created!", "id": new_id}
 
 
 
